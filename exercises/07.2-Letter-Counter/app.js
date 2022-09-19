d4e91c456600324e2310c16143628dc01786d3c8
David Suarez <dsuarez001@att.net>
2022-09-19 23:53:55 +0000

    partial save

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -1,34 +1,13 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
-let counts = [];
-let letters = {};
 let index = -1;
 
 // your code here
-letters = Array.from(par.toLowerCase());
-
-for(let i = 0; i < letters.length; i++)
-{
-    //do not count empty strings
-    if (letters[i] != ' ')
-    {
-        let obj = letters.find(o => o.LetterCode == letters[i]);
-
-         //if -1 then it is not found
-         if (index === -1)
-         {
-            let letterCounter = {
-                LetterCode: letters[i],
-                LetterCounter: 1
-            }
-
-            counts.push(letterCounter);
-         }
-         else
-         {
-            let foundLetter = letters[index];
-            foundLetter.letterCounter ++;
-         }
-    }
+let counts = {};
+for(let i in par){
+    const letter = par[i].toLowerCase();
+    if(letter == ' ' || letter == '') continue;
+    if(typeof(counts[letter]) === 'undefined') counts[letter] = 1;
+    else counts[letter]++;
 }
 
-//console.log(counts);
+console.log(counts);

9a9cf1ec94d67cfe0548e529bc6764cad066f164
David Suarez <dsuarez001@att.net>
2022-09-20 00:52:44 +0000

    partial save

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,27 @@
 let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
 
 // your code here
+function mergeTwoList(listOfNumbers)
+{
+    let odd = [];
+    let even = [];
+    let returnVal = [];
 
+    for(let i = 0; i < listOfNumbers.length; i++)
+    {
+        if (listOfNumbers[i] % 2 === 0)
+        {
+            even.push(listOfNumbers[i]);
+        }
+        else
+        {
+            odd.push(listOfNumbers[i]);
+        }
+    }
+
+    returnVal = odd.concat(even);
+    
+    return returnVal;
+}
 
 console.log(mergeTwoList(list_of_numbers))

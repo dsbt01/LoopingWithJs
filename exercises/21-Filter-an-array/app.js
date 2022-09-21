fbac1211d76d47e7cb06466a02df3c06a79c82e7
David Suarez <dsuarez001@att.net>
2022-09-21 19:36:25 -0400

    21

--- exercises/21-Filter-an-array/app.js
+++ exercises/21-Filter-an-array/app.js
@@ -1,5 +1,9 @@
 let allNames = ["Romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patricia", "Danzalee"];
 
 //your code here
+let resultingNames = allNames.filter(function(aName)
+{
+    return aName.toLowerCase().startsWith('r');
+});
 
 console.log(resultingNames);

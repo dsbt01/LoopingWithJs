cb9615ca641dab7bb25383cf3be23d5fa08dd641
David Suarez <dsuarez001@att.net>
2022-09-21 18:55:24 -0400

    partial save

--- exercises/20.2-More-mapping/app.js
+++ exercises/20.2-More-mapping/app.js
@@ -2,6 +2,10 @@
 
 var myNumbers = [23,234,345,4356234,243,43,56,2];
 
-// your code here
+myFunction = function (number) {
+    return number * 3; 
+}
+
+let newArray = myNumbers.map(myFunction); 
 
 console.log(newArray);

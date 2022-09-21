c459845bd9f9c948dfdd5d254e90316fd55ec003
David Suarez <dsuarez001@att.net>
2022-09-21 19:28:20 -0400

    partial save

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,16 @@
 let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
 
 //your code here
+let convertValue = function(value)
+{
+    if (value == '0')
+    {
+        return 'woko';
+    }
+    else{
+        return 'wiki';
+    }
+    
+}
+
+console.log(theBools.map(convertValue));

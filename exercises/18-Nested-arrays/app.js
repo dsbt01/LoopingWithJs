41ac9bb3129636875bbca53a1783f93856b858bf
David Suarez <dsuarez001@att.net>
2022-09-21 17:40:21 -0400

    partial save

--- exercises/18-Nested-arrays/app.js
+++ exercises/18-Nested-arrays/app.js
@@ -1,1 +1,11 @@
 var coordinatesArray = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]];
+
+function  PrintLonOnly(coorArray)
+{
+    coorArray.forEach(function(item,index)
+    {
+        console.log(item[1]);
+    });
+}
+
+PrintLonOnly(coordinatesArray);

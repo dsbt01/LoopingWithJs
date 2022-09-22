f4ac1e6bd0ac012f81f26f4727b10a30529eaee1
David Suarez <dsuarez001@att.net>
2022-09-21 20:53:50 -0400

    25

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,5 +1,32 @@
 // Add your code here
+function lyricsGenerator(arr)
+{
+    let retVal = '';
+    let counterOfOne = 0;
 
+    for(let i = 0; i < arr.length; i++)
+    {
+        if (arr[i] == 1)
+        {
+            counterOfOne = counterOfOne + 1;
+            retVal = retVal + 'Drop the base ';
+        }
+        
+        if (arr[i] == 0)
+        {
+            counterOfOne = 0;
+            retVal = retVal + 'Boom ';
+        }
+
+        if (counterOfOne == 3)
+        {
+            counterOfOne = 0;
+            retVal = retVal + '!!!Break the base!!! ';
+        }
+    }
+
+    return retVal;
+}
 
 // Don't change anything bellow this line
 console.log(lyricsGenerator([0,0,1,1,0,0,0]))

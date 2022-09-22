bbee1b4a2a33dd36bce000821256be80baec115c
David Suarez <dsuarez001@att.net>
2022-09-21 20:06:12 -0400

    22

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,24 @@
 // Code goes here
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 
+function matrixBuilder(value)
+{
+    let retVal = [];
 
+    for(let i = 0; i < value; i++)
+    {
+        var newArray = [];
+        for (let j = 0; j < value; j++)
+        {
+            newArray.push(randomIntFromInterval(0,1));
+        }
+        retVal.push(newArray);
+    }
+
+    return retVal;
+}
 
 // do not change anything from this line down
 console.log(matrixBuilder(5))

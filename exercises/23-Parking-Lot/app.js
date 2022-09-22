6ed23e095cb01ac453f87791d0accd1a40f8b962
David Suarez <dsuarez001@att.net>
2022-09-21 20:37:11 -0400

    23

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -8,6 +8,41 @@ let parking_state = [
 ]
 
 // Your code here
+function getParkingLotState(localArray)
+{
+  let arraySize = localArray.length;
 
+  let retVal = {
+    totalSlots: 0,
+     availableSlots: 0,
+     occupiedSlots: 0
+  }; 
+
+  for (let i = 0; i < arraySize; i++)
+  {
+    let innerArray = localArray[i];
+
+    for (let j = 0; j < innerArray.length; j++)
+    {
+      //do nothing for 0
+
+      //1 occupied
+      //2 available
+      if (innerArray[j] == 1)
+      {
+        retVal.totalSlots ++;
+        retVal.occupiedSlots ++;
+      }
+
+      if (innerArray[j] == 2)
+      {
+        retVal.totalSlots ++;
+        retVal.availableSlots ++;
+      }
+    }
+  }
+
+  return retVal;
+}
 
 console.log(getParkingLotState(parking_state))
